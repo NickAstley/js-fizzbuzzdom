ba1276927aa1c1f047d1e57fa1dd12282f42877a
NickAstley <andreanicastro1994@gmail.com>
2022-05-20 14:00:26 +0200

    Completed remaining milestones

--- js/main.js
+++ js/main.js
@@ -13,20 +13,34 @@ Consigli del giorno:
 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano” */
 
+// Creo una variabile che fa riferimento al contenitore che dovrà contenere i blocchi colorati con i numeri
+const numbersContainer = document.getElementById("contenitoreNumeri");
 
-//Stampiamo i numeri da 1 a 100 all'avvio della pagina
+// Inizio a contare
 for (let i = 1; i <= 100; i++) {
     // Il testo da stampare sarà uguale al contatore
     let testoDaStampare = i.toString();
+    // Creo una variabile per impostare una classe che dia lo sfondo ai singoli div. Di default sarà blu
+    let sfondoDiv = "bg-blue";
     if (i % 3 === 0 && i % 5 === 0) {
-        // Per i multipli di 3 E di 5, modifico il testo da stampare in "FizzBuzz"
+        // Per i multipli di 3 E di 5, modifico il testo da stampare in "FizzBuzz" e il colore di sfondo in rosso
         testoDaStampare = "FizzBuzz";
+        sfondoDiv = "bg-red";
     }else if (i % 3 === 0) {
-        // Per i multipli solo di 3, modifico il testo da stampare in "Fizz"
+        // Per i multipli solo di 3, modifico il testo da stampare in "Fizz" e il colore di sfondo in verde
         testoDaStampare = "Fizz";
+        sfondoDiv = "bg-green";
     } else if (i % 5 === 0) {
-        // Per i multipli solo di 5, modifico il testo da stampare in "Buzz"
+        // Per i multipli solo di 5, modifico il testo da stampare in "Buzz" e il colore di sfondo in giallo
         testoDaStampare = "Buzz";
+        sfondoDiv = "bg-yellow";
     }
+    //Stampiamo i numeri da 1 a 100 all'avvio della pagina
     console.log(testoDaStampare);
+    // Per ogni numero creo un div per la colonna. Al suo interno inserisco un div con delle classi Bootstrap per allinearne il contenuto e aggiungo la classe per il colore
+    numbersContainer.innerHTML += ` <div>
+                                        <div class="row justify-content-center align-items-center square fw-bold ${sfondoDiv}">
+                                            ${testoDaStampare}
+                                        </div>
+                                    </div>`;
 }
